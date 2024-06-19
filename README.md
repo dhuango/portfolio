c5ad326bdf728e47fd115f1c4891cfd01e124bde
David Huang <7davidhuang@gmail.com>
2024-06-18 19:21:10 -0500

    portfolio

--- README.md
+++
@@ -1,1 +0,0 @@
-A simple portfolio displaying my skills and projects
--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+let menuIcon = document.querySelector("#menu-cion");
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
+            })
+        }
+    }
+    )
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
 }
